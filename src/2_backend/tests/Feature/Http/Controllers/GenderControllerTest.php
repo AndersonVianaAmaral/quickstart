db2a01199e09eebe903907866d1762e96a40cc1b
Anderson <anderson.viana.amaral@gmail.com>
2020-11-13 11:10:44 -0300

    added gender tests

--- src/2_backend/tests/Feature/Http/Controllers/GenderControllerTest.php
+++ src/2_backend/tests/Feature/Http/Controllers/GenderControllerTest.php
@@ -2,45 +2,52 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use App\Models\Category;
+use App\Models\Gender;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
-class CategoryControllerTest extends TestCase
+class GenderControllerTest extends TestCase
 {
     use DatabaseMigrations;
-    
-    public function MakeCategory() {
-        $category = Category::create(['name'=>'XPTO', 'is_active'=>true]);
-        $category->refresh();
-        return $category;
+
+    public function MakeGender()
+    {
+        $gender = Gender::create(['name'=>'Gender X']);
+        $gender->refresh();
+        return $gender;
     }
 
-    public function testIndex()
+    public function testGetAll()
     {
-        $category = $this->MakeCategory();
-
-        $response = $this->get(route('categories.index'));
-
+        $gender = $this->MakeGender();
+        $response = $this->get(route('genders.index'));
         $response
             ->assertStatus(200)
-            ->assertJson([$category->toArray()]);
+            ->assertJson([$gender->toArray()]);
     }
 
     public function testShow()
     {
-        $category = $this->MakeCategory();
+        $gender = $this->MakeGender();
 
-        $response = $this->get(route('categories.show',['category'=>$category->id]));
+        $response = $this->get(route('genders.show',['gender'=>$gender->id]));
 
         $response
             ->assertStatus(200)
-            ->assertJson($category->toArray());
+            ->assertJson($gender->toArray());
     }
-    
+
+    public function testDelete(){
+        $this->MakeGender();
+        $gender = $this->MakeGender();
+
+        $response = $this->json('DELETE', route('genders.destroy',['gender'=> $gender->id]),[$gender]);
+        $response->assertStatus(204);
+    }
+
     public function testValidatorData()
     {
-        $response = $this->json('POST',route('categories.store'),[]);
+        $response = $this->json('POST',route('genders.store'),[]);
         
         $response
             ->assertStatus(422)
@@ -48,13 +55,13 @@ class CategoryControllerTest extends TestCase
             ->assertJsonMissingValidationErrors(['is_active'])
             ->assertJsonFragment([\Lang::get('validation.required',['attribute'=>'name'])]);
         
-        $response = $this->json('POST',route('categories.store'),['name'=>str_repeat('a',254), 'is_active'=>'a']);
+        $response = $this->json('POST',route('genders.store'),['name'=>str_repeat('a',254), 'is_active'=>'a']);
         $response
             ->assertStatus(422)
             ->assertJsonValidationErrors(['is_active'])
             ->assertJsonFragment([\Lang::get('validation.boolean',['attribute'=>'is active'])]);      
 
-        $response = $this->json('PUT',route('categories.update',['category'=>$this->MakeCategory()->id]),['is_active'=>'a']);
+        $response = $this->json('PUT',route('genders.update',['gender'=>$this->MakeGender()->id]),['is_active'=>'a']);
         $response->assertStatus(422)
             ->assertJsonValidationErrors(['name','is_active'])
             ->assertJsonFragment([
@@ -67,8 +74,8 @@ class CategoryControllerTest extends TestCase
 
     public function testSotre()
     {
-        $category = $this->MakeCategory();
-        $response = $this->json('PUT', route('categories.update',['category'=> $category->id]),[
+        $gender = $this->MakeGender();
+        $response = $this->json('PUT', route('genders.update',['gender'=> $gender->id]),[
             'name' => 'test',
             'is_active' => false
         ]);
@@ -80,42 +87,30 @@ class CategoryControllerTest extends TestCase
             ]);
         $this->assertFalse($response->json('is_active')); 
 
-        $response = $this->json('POST', route('categories.store'),[
+        $response = $this->json('POST', route('genders.store'),[
             'name' => 'test'
         ]);
 
-        $category = Category::find($response->json('id'));
+        $gender = Gender::find($response->json('id'));
         
         $response
             ->assertStatus(201)
-            ->assertJson($category->toArray());
+            ->assertJson($gender->toArray());
         $this->assertTrue($response->json('is_active'));
-        $this->assertNull($response->json('description'));
 
-        $response = $this->json('POST', route('categories.store'),[
+        $response = $this->json('POST', route('genders.store'),[
             'name' => 'test',
-            'description' => 'description',
             'is_active' => false
         ]);
 
-        $category = Category::find($response->json('id'));
+        $gender = Gender::find($response->json('id'));
 
         $response
         ->assertStatus(201)
-        ->assertJson($category->toArray())
+        ->assertJson($gender->toArray())
         ->assertJsonFragment([
-            'is_active' => false,
-            'description' => 'description'
+            'is_active' => false
         ]);
         $this->assertFalse($response->json('is_active'));
-        $this->assertNotNull($response->json('description'));
-    }
-
-    public function testDelete(){
-        $this->MakeCategory();
-        $category = $this->MakeCategory();
-
-        $response = $this->json('DELETE', route('categories.destroy',['category'=> $category->id]),[$category]);
-        $response->assertStatus(204);
     }
 }
